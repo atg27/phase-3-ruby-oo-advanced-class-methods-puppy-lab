762b26126b82507ab76a665ae631c3d9a62e54c4
Andrew <atg27@uw.edu>
2021-05-20 15:12:35 -0700

    commit tree 5ae97dd6c70a7b390b4d7f37ea1109953dfa5fc9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+require 'pry'
+class Dog
+
+    @@all = []
+
+    attr_accessor :name
+
+def initialize(name)
+    @name = name
+    save
+
+end
+
+def self.all
+    @@all
+end
+
+def self.clear_all
+    @@all = []
+end
+
+def self.print_all
+   
+    @@all.map{|dog| puts dog.name} 
+    
+end
+
+def save
+    @@all << self
+end
+
+end
